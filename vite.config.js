fcfce2533d8544831e55c58719776c3a45034be0
Bartosz Kąca <kacabartosz2@gmail.com>
2025-10-30 15:13:43 +0100

    vite added

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    hmr: {
-      host: 'localhost',
-    },
-    watch: {
-      usePolling: true,
-    },
-  },
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-  ],
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
 });
