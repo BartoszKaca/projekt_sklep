4e9a760c95f606583c61038dc18839a136b04e85
Bartłomiej Posiadała <posia123123@gmail.com>
2025-11-20 09:23:59 +0100

    strona główna dalej

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
-        $this->middleware('auth');
+        // pozwól gościom zobaczyć stronę główną, chronij inne metody
+        $this->middleware('auth')->except('index');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        return view('home');
+        $featuredProducts = Product::where('is_active', 1)
+            ->where('is_featured', 1)
+            ->with(['primaryImage', 'category', 'reviews'])
+            ->take(8)
+            ->get();
+
+        $latestProducts = Product::where('is_active', 1)
+            ->with(['primaryImage', 'category'])
+            ->latest()
+            ->take(8)
+            ->get();
+
+        return view('home', compact('featuredProducts', 'latestProducts'));
     }
 }
